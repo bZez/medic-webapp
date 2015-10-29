cf4c3ffd39b843df0bdf9b6da53d3d93fbf4c4eb
Milan Andric <milan@medicmobile.org>
2015-10-29 10:14:23 -0200

    fixed bug where default response did not respect locale value.
    Issue: https://github.com/medic/medic-webapp/issues/1190
    
    I changed the default `locale` value on the doc.sms_message object
    literal.  Leave `locale` undefined unless it appears in the message/form
    submission.  If you define it then it will override locale value in app
    settings when responses are generated.
    
    Previously we were assining the ddoc.app_settings.locale property value
    to doc.sms_message.locale by default which is wrong because that setting
    now should only affect the language used in the web interface.  We
    should not assign any static value to doc.sms_message at record creation
    time unless it is requested in the report submission.

--- packages/kujua-sms/kujua-sms/utils.js
+++ packages/kujua-sms/kujua-sms/utils.js
@@ -354,7 +354,10 @@ function getLabel(field, locale) {
 
 
 function getLocale(record) {
-    return record.locale || (record.sms_message && record.sms_message.locale) || 'en';
+    return record.locale ||
+           (record.sms_message && record.sms_message.locale) ||
+           exports.info.locale ||
+           'en';
 }
 
 /*
